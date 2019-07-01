354fac670be4b9869b171a0fdd7d15063d094fdb
3v0k4 <riccardo.odone@gmail.com>
2019-07-01 10:17:52 +0200

    stack exec -- yesod add-handler

---
+++ src/Handler/Landing.hs
@@ -0,0 +1,6 @@
+module Handler.Landing where
+
+import Import
+
+getLandingR :: Handler Html
+getLandingR = error "Not yet implemented: getLandingR"
---
+++ test/Handler/LandingSpec.hs
@@ -0,0 +1,10 @@
+module Handler.LandingSpec (spec) where
+
+import TestImport
+
+spec :: Spec
+spec = withApp $ do
+
+    describe "getLandingR" $ do
+        error "Spec not implemented: getLandingR"
+
