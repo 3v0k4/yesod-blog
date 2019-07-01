ea3cd862893917d9a0de6f74ffae3daf0e8e658b
3v0k4 <riccardo.odone@gmail.com>
2019-07-01 17:29:52 +0200

    landing: isAuthorized

--- src/Foundation.hs
+++ src/Foundation.hs
@@ -171,6 +171,8 @@ instance Yesod App where
     -- delegate to that function
     isAuthorized ProfileR _ = isAuthenticated
 
+    isAuthorized LandingR _ = return Authorized
+
     -- This function creates static content files in the static folder
     -- and names them based on a hash of their content. This allows
     -- expiration dates to be set far in the future without worry of
